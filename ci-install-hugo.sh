1dcc8f3baded8d827b069a500be933f015e0dad9
root <root@AJACKSON-GAMING.localdomain>
2017-09-21 16:17:57 -0500

    clean spaces

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -1,9 +1,7 @@
 HUGO_VERSION=0.19
 HUGO_DOWNLOAD=hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
-
 set -x
 set -e
-
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
   wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/${HUGO_DOWNLOAD}
